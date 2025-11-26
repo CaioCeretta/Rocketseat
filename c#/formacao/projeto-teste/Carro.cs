d41a34c14acab3e25a59ba08fbf1d6f476f4e145
CaioCeretta <caioceretta@gmail.com>
2025-11-25 22:39:08 -0300

    feat: first classes and methods

---
+++ c#/formacao/projeto-teste/Carro.cs
@@ -0,0 +1,20 @@
+
+
+namespace HelloWorld;
+
+class Carro
+{
+  public void Ligar()
+  {
+    Console.WriteLine("Carro ligado!");
+  }
+
+  public void Desligar()
+  {
+    Console.WriteLine("Carro desligado!");
+  }
+
+
+
+
+}
