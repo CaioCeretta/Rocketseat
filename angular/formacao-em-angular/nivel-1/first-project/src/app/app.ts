7af6203f0f36cc3870ab01d61d1a00528e91d6fb
CaioCeretta <caioceretta@gmail.com>
2025-11-29 11:59:18 -0300

    chore: up to two-way data binding

--- angular/formacao-em-angular/nivel-1/first-project/src/app/app.ts
+++ angular/formacao-em-angular/nivel-1/first-project/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component } from "@angular/core";
 import { PropertyBinding } from "./property-binding/property-binding";
+import { MeuBotao } from "./meu-botao/meu-botao";
+import { EventBinding } from "./event-binding/event-binding";
+import { BotaoFlat } from "./botao-flat/botao-flat";
+import { TwoWayDataBinding } from "./two-way-data-binding/two-way-data-binding";
 
 @Component({
 	selector: "app-root",
-	imports: [PropertyBinding],
+	imports: [PropertyBinding, MeuBotao, EventBinding, BotaoFlat, TwoWayDataBinding],
 	templateUrl: "./app.html",
 	styleUrl: "./app.css",
 })
