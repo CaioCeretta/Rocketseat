d0031a99e326d25193733135ac7783af85f29316
CaioCeretta <caioceretta@gmail.com>
2025-12-08 23:51:42 -0300

    chore: up to upload cancel and progress colors

--- general-projects/widget-upload/web/src/components/upload-widget-upload-item.tsx
+++ general-projects/widget-upload/web/src/components/upload-widget-upload-item.tsx
@@ -1,16 +1,21 @@
 import * as Progress from "@radix-ui/react-progress";
 import { Download, ImageUp, Link2, RefreshCcw, X } from "lucide-react";
 import { motion } from "motion/react";
-import type { Upload } from "../store/uploads";
+import { type Upload, useUploads } from "../store/uploads";
 import { formatBytes } from "../utils/format-bytes.";
 import { Button } from "./ui/button";
 
 interface UploadWidgetItemProps {
   upload: Upload;
+  uploadId: string;
 }
 
-export function UploadWidgetUploadItem(props: UploadWidgetItemProps) {
-  const { upload } = props;
+export function UploadWidgetUploadItem({
+  upload,
+  uploadId,
+}: UploadWidgetItemProps) {
+  const cancelUpload = useUploads((store) => store.cancelUpload);
+
   return (
     <motion.div
       className="p-3 rounded-lg flex flex-col gap-3 shadow-shape-content bg-white/[0.02]
@@ -28,20 +33,29 @@ export function UploadWidgetUploadItem(props: UploadWidgetItemProps) {
         </span>
 
         <span className="text-xxs text-zinc-400 flex gap-1.5 items-center">
-          <span className="line-through ">
-            {formatBytes(upload.file.size)}
-          </span>
+          <span className="line-through ">{formatBytes(upload.file.size)}</span>
           <div className="size-1 rounded-full bg-zinc-700" />
           <span>
             300KB
-            <span className="text-green-400 ml-1">- 94$</span>
+            <span className="text-green-400 ml-1">- 94%</span>
           </span>
           <div className="size-1 rounded-full bg-zinc-700" />
           <span>43%</span>
+          {upload.status === "progress" && <span>45%</span>}
+          {upload.status === "error" && <span className="text-red-400">Error</span>}
+          {upload.status === "canceled" && <span className="text-yellow-400">Canceled</span>}
         </span>
 
-        <Progress.Root className="bg-zinc-800 rounded-full h-1 overflow-hidden">
-          <Progress.Indicator className="bg-indigo-500 h-1 w-[43%]" />
+        <Progress.Root
+          className="group bg-zinc-800 rounded-full h-1 overflow-hidden"
+          data-status={upload.status}
+        >
+          <Progress.Indicator
+            className="bg-indigo-500 h-1 w-[43%]
+             group-data-[status=success]:bg-green-400
+             group-data-[status=canceled]:bg-yellow-400
+             group-data-[status=error]:bg-red-400"
+            style={{ width: upload.status === 'progress' ? '43%' : '100%' }} />
         </Progress.Root>
 
         <div className="absolute top-2.5 right-2.5 flex items-center gap-1">
@@ -59,7 +73,7 @@ export function UploadWidgetUploadItem(props: UploadWidgetItemProps) {
             <span className="sr-only">Retry upload</span>
           </Button>
 
-          <Button size="small">
+          <Button size="small" onClick={() => cancelUpload(uploadId)}>
             <X className="size-4" strokeWidth="1.5" />
             <span className="sr-only">Cancel Upload</span>
           </Button>
