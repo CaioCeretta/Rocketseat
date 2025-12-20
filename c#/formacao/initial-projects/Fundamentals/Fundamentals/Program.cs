7a5fccde7e001ecda97102d1c9719b51d65b3afc
CaioCeretta <caioceretta@gmail.com>
2025-12-19 23:26:42 -0300

    chore: up to enum example

---
+++ c#/formacao/initial-projects/Fundamentals/Fundamentals/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HelloWord;
+
+class Program
+{
+	enum DifficultyLevel
+	{
+		Low = 0,
+		Medium = 1,
+		High = 2
+	}
+
+	static void Main()
+	{
+		DifficultyLevel level = DifficultyLevel.High;
+
+		int integerLevel = (int)level;
+
+		Console.WriteLine(level);
+		Console.WriteLine(integerLevel);
+	}
+
+}
---
+++ c#/formacao/initial-projects/legacy/chapter-2/date-types.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace ProjetoTeste2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateOnly day = new DateOnly(2025, 12, 15);
+
+            string dayInText = day.ToString("d MMMM yyyy", new CultureInfo("en-US"));
+
+            DateTime day1 = new DateTime(2025, 12, 15);
+
+            DateTime today = DateTime.Today;
+
+            Console.WriteLine(today);
+
+        }
+    }
+}
