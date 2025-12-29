40292cfceb0b81305edc6747940365fe904eccf0
CaioCeretta <caioceretta@gmail.com>
2025-12-28 23:11:43 -0300

    chore: modified service

---
+++ angular/formacao-em-angular/nivel-1/5-services-state-management/src/app/example-1/services/products.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+	providedIn: "root",
+})
+export class ProductsService {
+	products = [
+		{ id: 1, name: "Bicycle", price: "1000" },
+		{ id: 2, name: "Monitor", price: "800" },
+		{ id: 3, name: "Bicycle", price: "100" },
+	];
+
+	addProduct(id: number, name: string, price: string) {
+		this.products = [...this.products, { id, name, price }];
+	}
+
+	removeProduct(id: number) {
+		const newProducts = this.products.filter((product) => product.id !== id);
+
+		this.products = newProducts;
+	}
+
+	removeFirst(id: number) {
+		const newProducts = this.products.filter((product) => product.id !== id);
+
+		this.products = newProducts;
+	}
+}
