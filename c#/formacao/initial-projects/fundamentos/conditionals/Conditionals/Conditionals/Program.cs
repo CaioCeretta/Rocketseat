b5bd09e5b4afaf5ae66e2bcbadd80d0401dadf53
CaioCeretta <caioceretta@gmail.com>
2026-01-02 22:28:19 -0300

    feat: conditionals code

---
+++ c#/formacao/initial-projects/fundamentos/conditionals/Conditionals/Conditionals/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Conditionals
+{
+    class Program
+    {
+        enum Colors
+        {
+            Red,
+            Blue,
+            Yellow
+        }
+
+        static void Main(string[] args)
+        {
+            int number = 10;
+
+            string result = number switch
+            {
+                7 => "Caio",
+                1 => "André",
+                10 => "Alex",
+                _ => "Unknown name"
+            };
+
+            Console.WriteLine(result);
+        }
+    }
+}
