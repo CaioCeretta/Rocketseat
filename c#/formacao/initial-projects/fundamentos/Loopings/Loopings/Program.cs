9a700b883250dd7c03c165b3788068375cb88eb3
CaioCeretta <caioceretta@gmail.com>
2026-01-03 01:26:32 -0300

    Up to continue loop

--- c#/formacao/initial-projects/fundamentos/Loopings/Loopings/Program.cs
+++ c#/formacao/initial-projects/fundamentos/Loopings/Loopings/Program.cs
@@ -4,29 +4,52 @@
     {
         static void Main(string[] args)
         {
-            var list = new List<string> { "Caio", "Alex", "André" };
-            var dictionary = new Dictionary<string, string>();
 
-            dictionary.Add("Name1", "Caio");
-			dictionary.Add("Name2", "Alex");
-			dictionary.Add("Name3", "André");
+
+   //         var list = new List<string> { "Caio", "Alex", "André" };
+   //         var dictionary = new Dictionary<string, string>();
+
+   //         dictionary.Add("Name1", "Caio");
+			//dictionary.Add("Name2", "Alex");
+			//dictionary.Add("Name3", "André");
 
 			//for(int i = 0; i < list.Count; i++)
 			//{
 			//    Console.WriteLine(list[i]);
 			//}
 
-            foreach (var item in dictionary)
-            {
-                Console.WriteLine(item);
-            }
+   //         foreach (var item in dictionary)
+   //         {
+   //             Console.WriteLine(item);
+   //         }
 
-			foreach (var name in list)
-            {
-                Console.WriteLine(name);
-            }
+			//foreach (var name in list)
+   //         {
+   //             Console.WriteLine(name);
+   //         }
+			var number = 0;
+
+			while (number < 100)
+			{
+
+				if (number == 50)
+				{
+					//break;
+					return;
+				}
+
+				Console.WriteLine($"Test {number}");
+				number++;
+
+			}
+
+			//do
+			//{
+			//	Console.WriteLine("Test");
+			//	number++;
+			//} while (number < 3);
 
 
-        }
+		}
     }
 }
--- c#/formacao/initial-projects/theory/loopings-theory.md
+++ c#/formacao/initial-projects/theory/loopings-theory.md
@@ -8,4 +8,27 @@ When we map over dictionaries using foreach we use the syntax
 foreach(element in dictionary) {...} where each element is a key, value pair
 if we want to print only the keys we use element.key and the same for the values
 
+### Continue keyword
+
+When we use `break;`, we want to exit the loop and continue on the program processing. When we use continue, we only
+want to stop it right there and keep the process on the next iteration. However, if we do something like
+
+```cs
+			while (number < 100)
+			{
+
+				if (number == 50)
+				{
+					//break;
+					continue;
+				}
+
+				Console.WriteLine($"Test {number}");
+				number++;
+
+			}
+```
+
+The number will reach 50 and continue, but it does not see the number increment to start on 51. Therefore, if we put
+the number++ at the bottom of the file, the program will break on this flow.
 
