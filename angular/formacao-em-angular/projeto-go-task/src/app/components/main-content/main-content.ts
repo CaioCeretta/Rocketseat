87ae59a210d5d5d5ff1b45184dcefb43b509313f
CaioCeretta <caioceretta@gmail.com>
2026-01-05 22:57:39 -0300

    feat: welcome and main components

--- angular/formacao-em-angular/projeto-go-task/src/app/components/main-content/main-content.ts
+++ angular/formacao-em-angular/projeto-go-task/src/app/components/main-content/main-content.ts
@@ -1,11 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { WelcomeSection } from "../welcome-section/welcome-section";
 
 @Component({
-  selector: 'app-main-content',
-  imports: [],
-  templateUrl: './main-content.html',
-  styleUrl: './main-content.css',
+	selector: "app-main-content",
+	imports: [WelcomeSection],
+	templateUrl: "./main-content.html",
+	styleUrl: "./main-content.css",
 })
-export class MainContent {
-
-}
+export class MainContent {}
