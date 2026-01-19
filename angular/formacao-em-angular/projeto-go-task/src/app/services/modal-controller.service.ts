309c409453a5d1587f5ead60b49827e6494443fe
CaioCeretta <caioceretta@gmail.com>
2026-01-18 23:55:32 -0300

    chore: modal service

---
+++ angular/formacao-em-angular/projeto-go-task/src/app/services/modal-controller.service.ts
@@ -0,0 +1,34 @@
+import { Dialog } from "@angular/cdk/dialog";
+import { Injectable, inject } from "@angular/core";
+import { TaskCommentsModal } from "../components/task-comments-modal/task-comments-modal";
+import { TaskFormModal } from "../components/task-form-modal/task-form-modal";
+
+@Injectable({
+	providedIn: "root",
+})
+export class ModalControllerService {
+	private readonly modalSizeOptions = {
+		maxWidth: "620px",
+		width: "95%",
+	};
+
+	private readonly _dialog = inject(Dialog);
+
+	openNewTaskModal() {
+		return this._dialog.open(TaskFormModal, {
+			...this.modalSizeOptions,
+		});
+	}
+
+	openEditTaskModal() {
+		return this._dialog.open(TaskFormModal, {
+			...this.modalSizeOptions,
+		});
+	}
+
+	openTaskCommentsModal() {
+\		return this._dialog.open(TaskFormModal, {
+			...this.modalSizeOptions,
+		});
+	}
+}
