85c38a7f6c4562a312741a39369d41ee81d59823
CaioCeretta <caioceretta@gmail.com>
2026-01-19 21:20:17 -0300

    chore: modal service tests

--- angular/formacao-em-angular/projeto-go-task/src/app/app.ts
+++ angular/formacao-em-angular/projeto-go-task/src/app/app.ts
@@ -1,15 +1,28 @@
-import { Component, signal } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { Header } from "./components/header/header";
 import { MainContent } from "./components/main-content/main-content";
 import { TaskCommentsModal } from "./components/task-comments-modal/task-comments-modal";
 import { TaskFormModal } from "./components/task-form-modal/task-form-modal";
+import { ModalControllerService } from "./services/modal-controller.service";
 
 @Component({
 	selector: "app-root",
-	imports: [Header, MainContent, TaskFormModal, TaskCommentsModal],
+	imports: [Header, MainContent],
 	templateUrl: "./app.html",
 	styleUrl: "./app.css",
 })
 export class App {
-	protected readonly title = signal("projeto-go-task");
+	private readonly _modalOpenControllerService = inject(ModalControllerService);
+
+	openTaskModal() {
+		this._modalOpenControllerService.openNewTaskModal();
+	}
+
+	openCommentsModal() {
+		this._modalOpenControllerService.openTaskCommentsModal();
+	}
+
+	openEditModal() {
+		this._modalOpenControllerService.openTaskCommentsModal();
+	}
 }
