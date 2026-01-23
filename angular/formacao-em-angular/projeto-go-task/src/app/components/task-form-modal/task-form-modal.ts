978168d0dbba25401bec7648adcdf865433e9fb5
CaioCeretta <caioceretta@gmail.com>
2026-01-23 00:37:30 -0300

    feat: modal component implementation

--- angular/formacao-em-angular/projeto-go-task/src/app/components/task-form-modal/task-form-modal.ts
+++ angular/formacao-em-angular/projeto-go-task/src/app/components/task-form-modal/task-form-modal.ts
@@ -1,14 +1,33 @@
 import { DIALOG_DATA } from "@angular/cdk/dialog";
 import { Component, inject } from "@angular/core";
+import {
+	FormControl,
+	FormGroup,
+	ReactiveFormsModule,
+	Validators,
+} from "@angular/forms";
 import type { ITaskModalData } from "../../interfaces/task-form-modal-data.interface";
 
 @Component({
 	selector: "app-task-form-modal",
-	imports: [],
+	imports: [ReactiveFormsModule],
 	templateUrl: "./task-form-modal.html",
 	styleUrl: "./task-form-modal.css",
 })
 export class TaskFormModal {
 	// Injects the data passed to the dialog when it was opened
 	readonly _data: ITaskModalData = inject(DIALOG_DATA);
+
+	taskForm: FormGroup = new FormGroup({
+		name: new FormControl(this._data.formValues.name ?? "", [
+			Validators.required,
+			Validators.minLength(10),
+		]),
+		description: new FormControl(this._data.formValues.description ?? "", [
+			Validators.required,
+			Validators.minLength(10),
+		]),
+	});
+
+	onFormSubmit() {}
 }
--- angular/formacao-em-angular/projeto-go-task/src/app/services/modal-controller.service.ts
+++ angular/formacao-em-angular/projeto-go-task/src/app/services/modal-controller.service.ts
@@ -33,7 +33,7 @@ export class ModalControllerService {
 			...this.modalSizeOptions,
 			data: {
 				mode: "edit",
-				...formValues,
+				formValues,
 			},
 		});
 	}
