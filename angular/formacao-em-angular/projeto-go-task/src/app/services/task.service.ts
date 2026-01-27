bdb6bea3f90c8832a320beb10aa78608b9e448e6
CaioCeretta <caioceretta@gmail.com>
2026-01-26 23:07:02 -0300

    chore: updated observable type

--- angular/formacao-em-angular/projeto-go-task/src/app/services/task.service.ts
+++ angular/formacao-em-angular/projeto-go-task/src/app/services/task.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
+import type { ITask } from "../interfaces/task.interface";
 
 @Injectable({
 	providedIn: "root",
 })
 export class TaskService {
 	//To-do tasks
-	private todoTasks$ = new BehaviorSubject<any[]>([]);
+	private todoTasks$ = new BehaviorSubject<ITask[]>([]);
 	readonly todoTasks = this.todoTasks$.asObservable();
 
 	//Doing task
-	private doingTasks$ = new BehaviorSubject<any[]>([]);
+	private doingTasks$ = new BehaviorSubject<ITask[]>([]);
 	readonly doingTasks = this.doingTasks$.asObservable();
 
 	// Completed tasks
-	private completedTasks$ = new BehaviorSubject<any[]>([]);
+	private completedTasks$ = new BehaviorSubject<ITask[]>([]);
 	readonly completedTasks = this.completedTasks$.asObservable();
 }
