f01029e4ab7e37008ac3431a7d84ef9c254dd008
CaioCeretta <caioceretta@gmail.com>
2026-01-27 23:33:39 -0300

    chore: function to visualize the current list

--- angular/formacao-em-angular/projeto-go-task/src/app/services/task.service.ts
+++ angular/formacao-em-angular/projeto-go-task/src/app/services/task.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 import type { ITask } from "../interfaces/task.interface";
+import type { ITaskFormControls } from "../interfaces/task-form-controls.interface";
+import { TaskStatusEnum } from "../types/task-status";
+import { generateUniqueIdWithTimeStamp } from "../utils/generate-unique-id-with-timestamp";
 
 @Injectable({
 	providedIn: "root",
 })
 export class TaskService {
 	//To-do tasks
-	private todoTasks$ = new BehaviorSubject<ITask[]>([]);
+	private todoTasks$ = new BehaviorSubject<ITask[]>([
+		{
+			id: "1234",
+			comments: [],
+			name: "jdasjdiosajdia",
+			description: "jdioasjdiakosjdioas",
+			status: TaskStatusEnum.TODO,
+		},
+	]);
 	readonly todoTasks = this.todoTasks$.asObservable();
 
 	//Doing task
@@ -17,4 +28,22 @@ export class TaskService {
 	// Completed tasks
 	private completedTasks$ = new BehaviorSubject<ITask[]>([]);
 	readonly completedTasks = this.completedTasks$.asObservable();
+
+	//addTask method
+	addTasks(taskInfos: ITaskFormControls) {
+		const newTask: ITask = {
+			id: generateUniqueIdWithTimeStamp(),
+			...taskInfos,
+			status: TaskStatusEnum.TODO,
+			comments: [],
+		};
+
+		const currentList = this.todoTasks$.value;
+
+		this.todoTasks$.next([...currentList, newTask]);
+	}
+
+	carregarListaAtualDeToDos() {
+		console.log(this.todoTasks$.value);
+	}
 }
