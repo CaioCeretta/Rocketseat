2b41970112032a55557872b84ea063e95cce5f3f
CaioCeretta <caioceretta@gmail.com>
2026-01-27 23:35:09 -0300

    chore: temporarily modifying a property via component

--- angular/formacao-em-angular/projeto-go-task/src/app/components/task-list-section/task-list-section.ts
+++ angular/formacao-em-angular/projeto-go-task/src/app/components/task-list-section/task-list-section.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import type { ITask } from "../../interfaces/task.interface";
+import type { ITaskFormControls } from "../../interfaces/task-form-controls.interface";
+import { TaskService } from "../../services/task.service";
+import { TaskStatusEnum } from "../../types/task-status";
+import { generateUniqueIdWithTimeStamp } from "../../utils/generate-unique-id-with-timestamp";
 import { TaskCard } from "../task-card/task-card";
 
 @Component({
-  selector: 'app-task-list-section',
-  imports: [TaskCard],
-  templateUrl: './task-list-section.html',
-  styleUrl: './task-list-section.css',
+	selector: "app-task-list-section",
+	imports: [TaskCard],
+	templateUrl: "./task-list-section.html",
+	styleUrl: "./task-list-section.css",
 })
 export class TaskListSection {
+	private readonly _taskService = inject(TaskService);
 
+	private currentList: ITask[] = [];
+
+	ngOnInit() {
+		this._taskService.todoTasks.subscribe((todoList) => {
+			this.currentList = todoList;
+
+			todoList[0].name = "Nome alterado";
+
+			this._taskService.carregarListaAtualDeToDos();
+		});
+	}
 }
