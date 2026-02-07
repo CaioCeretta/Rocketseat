2405d36801372ee99c5ae209cdb4cd6d9f23c92c
CaioCeretta <caioceretta@gmail.com>
2026-02-06 23:58:46 -0300

    chore: Retrieving data from the appsettings example

--- c#/formacao/API/MyFirstAPI/Program.cs
+++ c#/formacao/API/MyFirstAPI/Program.cs
@@ -14,6 +14,15 @@ namespace MyFirstAPI
 			builder.Services.AddEndpointsApiExplorer();
 			builder.Services.AddSwaggerGen();
 
+            //         var test = builder.Configuration.GetSection("Object").GetSection("Prop1").Value;
+            //test = builder.Configuration.GetSection("Object").GetSection("Prop2").Value;
+            //test = builder.Configuration.GetSection("Object").GetSection("PropA").Value;
+
+            //var test = builder.Configuration.GetSection("MyClass").GetSection("Number").Get<int>();
+
+            var test = builder.Configuration.GetValue<int>("MyClass:Number");
+
+
 
 			var app = builder.Build();
 
