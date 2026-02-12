780ee1e8ba07eab56553b44304e7e2830b6404a0
CaioCeretta <caioceretta@gmail.com>
2026-02-12 00:05:52 -0300

    chore: user controller

--- c#/formacao/API/MyFirstAPI/Controllers/UserController.cs
+++ c#/formacao/API/MyFirstAPI/Controllers/UserController.cs
@@ -1,23 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 namespace MyFirstAPI.Controllers
-
+     
 {
     [Route("api/[controller]")]
     [ApiController]
     public class UserController : ControllerBase
     {
         [HttpGet]
-        public IActionResult GET()
+        [Route("{id}")]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        public IActionResult GET(int id, string nickname)
         {
-            var response = new Response
-            {
+            var response = new User
+            { 
+                Id = 1,
                 Name = "Caio",
-                Age = 29
+                Age = 29        
             };
 
-            return Ok(response);
+            return Ok(response); 
         }
              
     }
 }
+ 
