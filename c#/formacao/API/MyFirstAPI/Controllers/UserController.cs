a7efdde1413dbbaea057acea2cab2a879c636dd9
CaioCeretta <caioceretta@gmail.com>
2026-02-16 20:49:31 -0300

    chore: similar name endpoints

--- c#/formacao/API/MyFirstAPI/Controllers/UserController.cs
+++ c#/formacao/API/MyFirstAPI/Controllers/UserController.cs
@@ -9,7 +9,26 @@ namespace MyFirstAPI.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+
+        //Query Params Example
+        //[HttpGet]
+        //[ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        //public IActionResult GetQueryParams(int id, string nickname)
+        //{
+        //    var response = new User
+        //    {
+        //        Id = 1,
+        //        Name = "Caio",
+        //        Age = 29,
+        //    };
+
+        //    return Ok("Caio");
+          
+        //}
+
+        // Route Params Example
         [HttpGet]
+        [Route("{id}")]
         [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
         public IActionResult GetById([FromHeader] int id, [FromHeader] string nickname)
@@ -40,13 +59,54 @@ namespace MyFirstAPI.Controllers
         }
 
         [HttpPut]
-        [Route("{id}")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
         public IActionResult Update([FromRoute] int id, [FromBody] RequestUpdateUserProfileJson request)
         {
             return NoContent();
         }
-             
-    }
+
+		[HttpDelete]
+		[Route("{id}")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		public IActionResult Delete([FromRoute] int id)
+		{
+			return NoContent();
+		}
+
+        [HttpGet]
+		[ProducesResponseType(typeof(List<User>), StatusCodes.Status200OK)]
+
+		public IActionResult GetAll()
+        {
+            var response = new List<User>()
+           {
+                new User { Id = 1, Age = 7, Name = "Blub"},
+			    new User { Id = 2, Age = 30, Name = "Caio"},
+
+		   };
+
+            return Ok();
+        }
+
+
+
+		[HttpPut("update-password")]
+		[Route("{id}")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		public IActionResult ChangePassword([FromRoute] int id, [FromBody] RequestChangePasswordJson request)
+		{
+			return NoContent();
+		}
+
+		[HttpPut("update-password2")]
+		[Route("{id}")]
+		[ProducesResponseType(StatusCodes.Status204NoContent)]
+		public IActionResult ChangePassword2([FromRoute] int id, [FromBody] RequestChangePasswordJson request)
+		{
+			return NoContent();
+		}
+
+
+	}
 } 
  
